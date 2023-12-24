6d3b53fee2141dc66c665ea7ebaa3813bd51ae80
Esteban Lopez <lopezestebanalejandro@gmail.com>
2023-12-23 22:15:22 -0300

    Creo rama ejjs2  - ejercicio2 documentwrite

---
+++ js/ejjs2.js
@@ -0,0 +1,5 @@
+
+// 2.- Escribe un programa de una 
+// sola línea que escriba en la pantalla un texto que diga «Hello World» (document.write).
+
+document.write('<p class = "text-center text-primary display-1">Hello World</p>')
