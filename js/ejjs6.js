3512fab035c9efe3c91ae055e23c7cb4449a0f4c
Esteban Lopez <lopezestebanalejandro@gmail.com>
2023-12-24 16:27:39 -0300

    ej 6 ingrese dos numeros y diga cual es el mayor programa

--- js/ejjs6.js
+++ js/ejjs6.js
@@ -2,3 +2,14 @@
 // Ejemplo: 
 // input: 15 , 3
 // Output: El 15 es el número más grande
+
+let numero1 = parseFloat(prompt("Ingrese el primer número:"));
+let numero2 = parseFloat(prompt("Ingrese el segundo número:"));
+
+if (numero1 > numero2) {
+  document.write("El " + numero1 + " es el número más grande.");
+} else if (numero2 > numero1) {
+  document.write("El " + numero2 + " es el número más grande.");
+} else {
+  document.write("Los números son iguales.");
+}
